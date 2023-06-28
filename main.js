df9c5b9fc3333fe66d863d76f61f7957ed5e48fa
Stepan Rudiak <stepan.rudiak95@gmail.com>
2023-06-28 23:22:04 +0200

    c1

---
+++ main.js
@@ -0,0 +1,21 @@
+document.querySelector('.menu').addEventListener('click', () => {
+  document.querySelectorAll('.target').forEach((item) => {
+    item.classList.toggle('active');
+  })
+})
+
+const icons = document.querySelectorAll('.section-1__icon')
+let i = 1
+
+setInterval(() => {
+  i++
+  const icon = document.querySelector('.section-1__icon.section-1__icon--selected')
+  icon.classList.remove('section-1__icon--selected')
+
+  if (i > icons.length) {
+    icons[0].classList.add('section-1__icon--selected')
+    i = 1
+  } else {
+    icon.nextElementSibling.classList.add('section-1__icon--selected')
+  }
+}, 2000)
